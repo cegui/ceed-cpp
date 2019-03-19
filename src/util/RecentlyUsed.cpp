1c00f2b11603a80ce060a46a9d8d9ecdde6c2dd9
Vladimir Orlov <niello@bk.ru>
2019-03-19 16:08:51 +0200

    CEED porting: recently used - main code

--- src/util/RecentlyUsed.cpp
+++ src/util/RecentlyUsed.cpp
@@ -1,6 +1,140 @@
 #include "src/util/RecentlyUsed.h"
+#include "src/Application.h"
+#include "src/util/Settings.h"
+#include "qsettings.h"
 
-RecentlyUsed::RecentlyUsed()
+RecentlyUsed::RecentlyUsed(const QString& sectionID, QObject* parent)
+    : QObject(parent)
+    , _sectionID("recentlyUsedIdentifier/" + sectionID)
+{
+}
+
+void RecentlyUsed::addRecentlyUsed(const QString& name)
+{
+    QStringList items;
+    getRecentlyUsed(items);
+
+    if (!items.contains(name))
+        items.insert(0, name);
+
+    while (items.size() > _maxItems)
+        items.pop_back();
+
+    auto settings = qobject_cast<Application*>(qApp)->getSettings()->getQSettings();
+    settings->setValue(_sectionID, items);
+}
+
+// Safe to call even if the item is not in the list
+bool RecentlyUsed::removeRecentlyUsed(const QString& name)
+{
+    QStringList items;
+    getRecentlyUsed(items);
+
+    if (!items.contains(name)) return false;
+
+    items.removeAll(name);
+    auto settings = qobject_cast<Application*>(qApp)->getSettings()->getQSettings();
+    settings->setValue(_sectionID, items);
+    return true;
+}
+
+void RecentlyUsed::clearRecentlyUsed()
+{
+    auto settings = qobject_cast<Application*>(qApp)->getSettings()->getQSettings();
+    settings->remove(_sectionID);
+}
+
+void RecentlyUsed::getRecentlyUsed(QStringList& outList) const
+{
+    auto settings = qobject_cast<Application*>(qApp)->getSettings()->getQSettings();
+    if (!settings->contains(_sectionID)) return;
+
+    outList = settings->value(_sectionID).toStringList();
+
+    //QString value = settings->value(_sectionID).toString();
+    // outList = value -> string list
+}
+
+//---------------------------------------------------------------------
+
+RecentlyUsedMenuEntry::RecentlyUsedMenuEntry(const QString& sectionID, QObject* parent)
+    : RecentlyUsed(sectionID, parent)
 {
 
+}
+
+void RecentlyUsedMenuEntry::setParentMenu(QMenu* menu)
+{
+    if (_menu == menu) return;
+
+    _menu = menu;
+/*
+        self.slot = slot
+        self.clearAction = clearAction
+        self.clearAction.triggered.connect(self.slot_clear)
+*/
+    updateMenu();
+}
+
+void RecentlyUsedMenuEntry::addRecentlyUsed(const QString& name)
+{
+    RecentlyUsed::addRecentlyUsed(name);
+    updateMenu();
+}
+
+bool RecentlyUsedMenuEntry::removeRecentlyUsed(const QString& name)
+{
+    const bool result = RecentlyUsed::removeRecentlyUsed(name);
+    updateMenu();
+    return result;
+}
+
+void RecentlyUsedMenuEntry::clearRecentlyUsed()
+{
+    RecentlyUsed::clearRecentlyUsed();
+    updateMenu();
+}
+
+void RecentlyUsedMenuEntry::recentItemActionTriggered()
+{
+    QAction* action = qobject_cast<QAction*>(sender());
+    if (action)
+        emit triggered(action->data().toString());
+}
+
+void RecentlyUsedMenuEntry::updateMenu()
+{
+    if (!_menu) return;
+
+    _menu->clear();
+
+    QStringList items;
+    getRecentlyUsed(items);
+
+    int i = 0;
+    for (auto& item : items)
+    {
+        QString text = item;
+
+        constexpr int _maxNameLength = 60;
+        if (text.length() > _maxNameLength)
+            text = "..." + text.right(_maxNameLength - 3);
+
+        if (i < 10)
+            text = QString("&%1. %2").arg((i + 1) % 10).arg(text);
+
+        auto action = new QAction(text, _menu);
+        action->setData(item);
+        action->setVisible(true);
+        connect(action, &QAction::triggered, this, &RecentlyUsedMenuEntry::recentItemActionTriggered);
+
+        ++i;
+    }
+
+    _menu->addSeparator();
+
+    auto action = new QAction("Clear", _menu);
+    action->setVisible(true);
+    action->setEnabled(!items.empty());
+    connect(action, &QAction::triggered, this, &RecentlyUsedMenuEntry::clearRecentlyUsed);
 }
--- src/util/RecentlyUsed.h
+++ src/util/RecentlyUsed.h
@@ -1,16 +1,69 @@
 #ifndef RECENTLYUSED_H
 #define RECENTLYUSED_H
 
+#include "qstring.h"
+#include "qmenu.h"
+
 // Implements reusable functionality for "recently used" lists/menus.
 // Can be used to store pointers to Items like files and images
 // for later reuse within the application.
 
 // Stefan Stammberger is the author of the original python implementation
 
-class RecentlyUsed
+// This class can be used to store pointers to Items like files and images
+// for later reuse within the application.
+class RecentlyUsed : public QObject
 {
+    Q_OBJECT
+
 public:
-    RecentlyUsed();
+
+    RecentlyUsed(const QString& sectionID, QObject* parent = nullptr);
+
+    virtual void addRecentlyUsed(const QString& name);
+    virtual bool removeRecentlyUsed(const QString& name);
+    void getRecentlyUsed(QStringList& outList) const;
+
+public slots:
+
+    virtual void clearRecentlyUsed();
+
+signals:
+
+    void triggered(const QString& itemName);
+
+protected:
+
+    QString _sectionID;
+    int _maxItems = 10;
+};
+
+// This class can be used to manage a Qt Menu entry to items
+class RecentlyUsedMenuEntry : public RecentlyUsed
+{
+    Q_OBJECT
+
+public:
+
+    RecentlyUsedMenuEntry(const QString& sectionID, QObject* parent = nullptr);
+
+    void setParentMenu(QMenu* menu);
+    virtual void addRecentlyUsed(const QString& name) override;
+    virtual bool removeRecentlyUsed(const QString& name) override;
+
+public slots:
+
+    virtual void clearRecentlyUsed() override;
+
+protected slots:
+
+    void recentItemActionTriggered();
+
+protected:
+
+    void updateMenu();
+
+    QMenu* _menu = nullptr;
 };
 
 #endif // RECENTLYUSED_H
